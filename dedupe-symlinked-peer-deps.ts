621cd0293c3cdbc700883ad4d0ff05828d0146ba
Ismail Habib Muhammad <ismail.muhammad@mendix.com>
2016-10-10 14:51:40 +0200

    No need to mark the files as node-executables

--- dedupe-symlinked-peer-deps.ts
+++ dedupe-symlinked-peer-deps.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import {exec} from "shelljs";
 import {
     getRequirements,
--- redupe-symlinked-peer-deps.ts
+++ redupe-symlinked-peer-deps.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import {exec} from "shelljs";
 import {
     executeIn, getTopLevelSymlinkedPackagesPath, getRequirements
